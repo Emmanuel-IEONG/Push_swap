f05d60c90fbac4a1c2ed4c83559dbc56a289dcd1
Emmanuel Ieong <eieong@made-f0Ar12s11.clusters.42paris.fr>
2025-03-25 13:34:26 +0100

    starting project parsing

--- src/push_swap.c
+++ src/push_swap.c
@@ -6,7 +6,7 @@
 /*   By: eieong <eieong@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/13 12:09:08 by eieong            #+#    #+#             */
-/*   Updated: 2025/03/19 12:40:57 by eieong           ###   ########.fr       */
+/*   Updated: 2025/03/25 13:33:20 by eieong           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,17 +25,55 @@ int	stack_sorted(t_stack *stack)
 	return (1);
 }
 
-int	main(int argc, char **argv)
+/*	if ac = 2
+		split, atoi, check int, check nb arg
+	if ac > 2
+		ac[i] : atoi, check int, -> stack*/
+
+/*gerer pb atoi ("-" = 0, INT_MIN)*/
+t_bool	check_one_arg(char **av, t_stack *a)
+{
+	char	**split;
+	int		i;
+
+	split = ft_split(av[1], ' ');
+	if (!split)
+		return (NULL);
+	if (split[0] && !split[1])
+		return (false);
+	while (split[i])
+	{
+		// stack.nb = ft_atoi(split[i]);
+		// stack.index = i;
+	}
+}
+
+t_bool	check_args(int ac, char **av, t_stack *a)
+{
+	int	i;
+
+	i = 0;
+	while (i++ < ac - 1)
+	{
+		// stack.nb = ft_atoi(av[i]);
+		// stack.index = i - 1;
+	}
+}
+
+int	main(int ac, char **av)
 {
 	t_stack	*a;
 	t_stack	*b;
 
 	a = NULL;
 	b = NULL;
-	if (argc == 1 || (!argv[1][0] && argc == 2))
+	if (ac == 1 || (!av[1][0] && ac == 2))
 		return (1);
-	else if (argc == 2)
-		argv = ft_split(argv[1], ' ');
+	else if (ac == 2)
+		check_one_arg(av, a);
+	else if (ac > 2)
+		check_args(ac, av, a);
+		
 	// ini a
 	if (!stack_sorted(a))
 	{
--- src/push_swap.h
+++ src/push_swap.h
@@ -6,12 +6,16 @@
 /*   By: eieong <eieong@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/13 12:03:47 by eieong            #+#    #+#             */
-/*   Updated: 2025/03/25 11:27:12 by eieong           ###   ########.fr       */
+/*   Updated: 2025/03/25 13:30:10 by eieong           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef PUSH_SWAP_H
 # define PUSH_SWAP_H
+
+#include "../libft/ft_printf.h"
+#include "../libft/get_next_line_bonus.h"
+#include "../libft/libft.h"
 
 typedef enum	e_bool
 {
